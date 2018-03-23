8dc22b755ee97f24a3f5c4d207cd8ef8714a52d7
canbaran <canbaran85@gmail.com>
2018-03-23 18:35:46 +0300

    CB:-zipping the .txt file

--- src/main/java/com/github/pires/obd/reader/io/ObdGatewayService.java
+++ src/main/java/com/github/pires/obd/reader/io/ObdGatewayService.java
@@ -37,8 +37,10 @@ import com.github.pires.obd.reader.io.BluetoothManager;
 import com.github.pires.obd.reader.io.ObdCommandJob.ObdCommandJobState;
 import com.google.inject.Inject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -56,6 +58,8 @@ import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -492,8 +496,14 @@ public class ObdGatewayService extends AbstractGatewayService {
         sb.append("\nRelease: ").append(Build.VERSION.RELEASE);
         sb.append("\nVin: ").append(vinNumber);
         sb.append("\nUserName: ").append(userName);
+        logProcess.destroy();
+        String[] temp = logFile.getAbsolutePath().split("\\.");
+        String toLocation = temp[0] + ".zip";
+        boolean zipped =  zipFileAtPath(logFile.getAbsolutePath(), toLocation);
 
-        String[] attachments = {logFile.getAbsolutePath()};
+        String attachmentString = (zipped) ? toLocation : logFile.getAbsolutePath();
+//        Log.d("TAG", "attachment String  "  + attachmentString);
+        String[] attachments = {attachmentString};
         String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
         String currentSubject = "System Logs from: " + currentDateTimeString;
         emailRunnable myEmailRunnable = new emailRunnable(attachments,currentDateTimeString,currentSubject, sb );
@@ -508,7 +518,7 @@ public class ObdGatewayService extends AbstractGatewayService {
         if (myEmailRunnable.getValue()) {
             actionResult.put("email", "Success");
             //now here delete the .txt file we just generated via killing  / destroying the process first
-            logProcess.destroy();
+
             boolean isDeleted = logFile.delete();
             Log.d(TAG, "LogFile Deleted");
 
@@ -547,7 +557,89 @@ public class ObdGatewayService extends AbstractGatewayService {
     }
 
 
+/*
+ *
+ * Zips a file at a location and places the resulting zip file at the toLocation
+ * Example: zipFileAtPath("downloads/myfolder", "downloads/myFolder.zip");
+ */
 
+    private static boolean zipFileAtPath(String sourcePath, String toLocation) {
+        final int BUFFER = 2048;
+
+        File sourceFile = new File(sourcePath);
+        try {
+            BufferedInputStream origin = null;
+            FileOutputStream dest = new FileOutputStream(toLocation);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(
+                    dest));
+            if (sourceFile.isDirectory()) {
+                zipSubFolder(out, sourceFile, sourceFile.getParent().length());
+            } else {
+                byte data[] = new byte[BUFFER];
+                FileInputStream fi = new FileInputStream(sourcePath);
+                origin = new BufferedInputStream(fi, BUFFER);
+                ZipEntry entry = new ZipEntry(getLastPathComponent(sourcePath));
+                out.putNextEntry(entry);
+                int count;
+                while ((count = origin.read(data, 0, BUFFER)) != -1) {
+                    out.write(data, 0, count);
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /*
+ *
+ * Zips a subfolder
+ *
+ */
+
+    private static void zipSubFolder(ZipOutputStream out, File folder,
+                              int basePathLength) throws IOException {
+
+        final int BUFFER = 2048;
+
+        File[] fileList = folder.listFiles();
+        BufferedInputStream origin = null;
+        for (File file : fileList) {
+            if (file.isDirectory()) {
+                zipSubFolder(out, file, basePathLength);
+            } else {
+                byte data[] = new byte[BUFFER];
+                String unmodifiedFilePath = file.getPath();
+                String relativePath = unmodifiedFilePath
+                        .substring(basePathLength);
+                FileInputStream fi = new FileInputStream(unmodifiedFilePath);
+                origin = new BufferedInputStream(fi, BUFFER);
+                ZipEntry entry = new ZipEntry(relativePath);
+                out.putNextEntry(entry);
+                int count;
+                while ((count = origin.read(data, 0, BUFFER)) != -1) {
+                    out.write(data, 0, count);
+                }
+                origin.close();
+            }
+        }
+    }
+
+    /*
+ * gets the last path component
+ *
+ * Example: getLastPathComponent("downloads/example/fileToZip");
+ * Result: "fileToZip"
+ */
+    private static String getLastPathComponent(String filePath) {
+        String[] segments = filePath.split("/");
+        if (segments.length == 0)
+            return "";
+        String lastPathComponent = segments[segments.length - 1];
+        return lastPathComponent;
+    }
 
 
     @DynamoDBTable(tableName = "canData")
